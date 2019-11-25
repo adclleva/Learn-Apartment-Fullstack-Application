41afb85a1f473f241e9f4ea9ab9ea829efab46b8
ArvinLleva <arvin.lleva@yahoo.com>
2019-11-25 14:52:58 -0800

    Finally got the React in Rails with Devise, and able to get something showing in the browser, also always remember to re-run your server

--- fullstack-apartment-app/config/routes.rb
+++ fullstack-apartment-app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  # root to: "home#index"
+  get '*path', to: 'pages#homepage', constraints: ->(request){ request.format.html? }
+  root to: 'pages#homepage'
 end
